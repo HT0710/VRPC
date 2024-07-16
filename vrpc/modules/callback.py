a0ffccde0abd4c104ed0b88d74cee96d9f1a5b36
ArtA <pthung7102002@gmail.com>
2024-07-16 20:44:18 +0700

    update requirements, callback

--- vrpc/modules/callback.py
+++ vrpc/modules/callback.py
@@ -1,57 +1,63 @@
 from typing import List
 from rich import print
-import yaml
 
 from lightning.pytorch import Trainer, LightningModule
 import lightning.pytorch.callbacks as cb
+
+from .utils import yaml_handler
 
 
 class PrintTrainResult(cb.Callback):
     def on_train_epoch_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
         epoch = trainer.current_epoch
+        results = trainer.callback_metrics
 
         current_lr = ", ".join(
-            [f"{optim.param_groups[0]['lr']:.2e}" for optim in trainer.optimizers]
+            f"{optim.param_groups[0]['lr']:.2e}" for optim in trainer.optimizers
         )
 
-        results = trainer.callback_metrics
-        result_train = (
+        train_result = (
             f"loss: {results['train/loss']:.4f}, acc: {results['train/accuracy']:.3f}"
         )
-        result_val = (
-            f"loss: {results['val/loss']:.4f}, acc: {results['val/accuracy']:.3f}"
-        )
 
-        print(
-            f"[bold]Epoch[/]( {epoch} ) "
-            f"[bold]Lr[/]( {current_lr} ) "
-            f"[bold]Train[/]({result_train}) "
-            f"[bold]Val[/]({result_val})"
-        )
+        output = [
+            f"[bold]Epoch[/]( {epoch} )",
+            f"[bold]Lr[/]( {current_lr} )",
+            f"[bold]Train[/]({train_result})",
+        ]
+
+        if "val/loss" in results:
+            val_result = (
+                f"loss: {results['val/loss']:.4f}, acc: {results['val/accuracy']:.3f}"
+            )
+            output.append(f"[bold]Val[/]({val_result})")
+
+        print(" ".join(output))
 
 
 def custom_callbacks() -> List[cb.Callback]:
     """
-    Configuration for custom callbacks
+    Configure and return a list of custom callbacks for PyTorch Lightning.
 
     Returns
     -------
-    List
-        A list of defined callbacks.
+    List[cb.Callback]
+        A list of configured PyTorch Lightning callback objects.
     """
-    with open("vrpc/configs/callbacks.yaml", "r") as file:
-        cfg = yaml.safe_load(file)
+    cfg = yaml_handler("vrpc/configs/callbacks.yaml")
     callbacks = []
-    if cfg["verbose"]:
-        callbacks.append(PrintTrainResult())
-    if cfg["model_summary"]:
-        callbacks.append(cb.RichModelSummary())
-    if cfg["progress_bar"]:
-        callbacks.append(cb.RichProgressBar())
-    if cfg["lr_monitor"]:
-        callbacks.append(cb.LearningRateMonitor("epoch"))
-    if cfg["enable_checkpoint"]:
-        callbacks.append(cb.ModelCheckpoint(**cfg["checkpoint"]))
-    if cfg["enable_early_stopping"]:
-        callbacks.append(cb.EarlyStopping(**cfg["early_stopping"]))
+
+    callback_map = {
+        "verbose": PrintTrainResult(),
+        "model_summary": cb.RichModelSummary(),
+        "progress_bar": cb.RichProgressBar(),
+        "lr_monitor": cb.LearningRateMonitor("epoch"),
+        "enable_checkpoint": cb.ModelCheckpoint(**cfg["checkpoint"]),
+        "enable_early_stopping": cb.EarlyStopping(**cfg["early_stopping"]),
+    }
+
+    for key, callback in callback_map.items():
+        if cfg.get(key, False):
+            callbacks.append(callback)
+
     return callbacks
