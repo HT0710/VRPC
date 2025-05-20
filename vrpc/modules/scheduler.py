3a291adb0d4c5c50373f9b09aa1e047fefbb29b5
ArtA <pthung7102002@gmail.com>
2025-05-20 07:31:18 +0700

    Update scheduler.py

--- vrpc/modules/scheduler.py
+++ vrpc/modules/scheduler.py
@@ -1,8 +1,7 @@
-from torch.optim.lr_scheduler import SequentialLR , LambdaLR
+from torch.optim.lr_scheduler import SequentialLR, LambdaLR
 from torch.optim.optimizer import Optimizer
 
 from bisect import bisect_right
-
 
 
 class SequentialLR(SequentialLR):
@@ -14,10 +13,20 @@ class SequentialLR(SequentialLR):
         self._last_lr = scheduler.get_last_lr()
 
 
-def scheduler_with_warmup(scheduler: Optimizer, warmup_epochs: int, start_factor: float=0.1):
-    warmup_scheduler = LambdaLR(scheduler.optimizer, lr_lambda=lambda epoch: start_factor + (1 - start_factor) * (epoch / warmup_epochs))
+def scheduler_with_warmup(
+    scheduler: Optimizer, warmup_epochs: int, start_factor: float = 0.1
+):
+    if warmup_epochs <= 1:
+        return scheduler
+
+    warmup_scheduler = LambdaLR(
+        scheduler.optimizer,
+        lr_lambda=lambda epoch: start_factor
+        + (1 - start_factor) * (epoch / warmup_epochs),
+    )
+
     return SequentialLR(
-        optimizer = scheduler.optimizer,
-        schedulers = [warmup_scheduler, scheduler],
-        milestones = [warmup_epochs+1]
+        optimizer=scheduler.optimizer,
+        schedulers=[warmup_scheduler, scheduler],
+        milestones=[warmup_epochs + 1],
     )
